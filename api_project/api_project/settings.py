c437e66c719845006980e28cb094bdd5a403fb26
stemsrd <n0610943@gmail.com>
2024-07-14 13:57:12 +0200

    Add databases dict

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -105,7 +105,12 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Use custom database wrapper
 DATABASES['default']['OPTIONS'] = {'threadlocals': True}
