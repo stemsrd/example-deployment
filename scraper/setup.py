ce7c67498aad03873f835c6f181c5880b5ab9cb0
stemsrd <n0610943@gmail.com>
2024-07-15 13:47:25 +0200

    Correct syntax

--- scraper/setup.py
+++ scraper/setup.py
@@ -5,11 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        urllib3<2.0
-        bs4>=0.0.1
-        psutil>=5.8.0
-        requests>=2.25.0
-        selenium>=4.0.0
-        webdriver-manager>=3.5.0
+        'bs4>=0.0.1',
+        'requests>=2.25.0',
+        'selenium>=4.0.0',
+        'webdriver-manager>=3.5.0',
     ],
 )
